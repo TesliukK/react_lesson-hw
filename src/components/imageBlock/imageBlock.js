85895e178b458c4633ac0b501b9c5296ba0e9e16
TesliukK <teslukkirill@gmail.com>
2023-01-17 17:52:19 +0200

    master

--- src/components/imageBlock/imageBlock.js
+++ src/components/imageBlock/imageBlock.js
@@ -4,7 +4,7 @@ export const ImageBlock = () => {
     const url = 'https://www.seekpng.com/png/detail/69-690313_bart-simpsons-png-bart-simpson-ass.png'
     return (
         <div>
-            <img src={url} alt="bart"/>
+            <img src={url} alt="bartdd"/>
         </div>
     );
 };
