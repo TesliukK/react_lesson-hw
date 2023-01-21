9fd63acac9a433b133700f96a648b5a7912f4101
TesliukK <teslukkirill@gmail.com>
2023-01-21 14:44:08 +0200

    spaceX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import {useEffect, useState} from "react";
 
 const App = () => {
+  const [launches, setLaunches] = useState([])
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/launches/')
+        .then(value => value.json())
+        .then(value => setLaunches(value))
+  })
   return (
     <div className="App">
-
+      {launches.map(item => {
+        return (
+            <div>
+              flight_number: {item.flight_number} - mission_name: {item.mission_name}
+            </div>
+        )
+      })}
     </div>
   );
 }
