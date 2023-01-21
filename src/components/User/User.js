fded0daae365b60d296395acb62fada487556d98
TesliukK <teslukkirill@gmail.com>
2023-01-21 13:37:47 +0200

    css

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import '..//style.css'
 
 const User = ({user, setUserDetails}) => {
     const {id,name,username,email} = user
     return (
-        <div>
+        <div className="block">
             <div>id: {id}</div>
             <div>name: {name}</div>
             <div>username: {username}</div>
             <div>email: {email}</div>
-            <button onClick={()=>setUserDetails(user)}>Details</button>
+            <button className="btn" onClick={()=>setUserDetails(user)}>Details</button>
         </div>
     );
 };
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import User from "../User/User";
 import {userService} from "../../services/userService";
+import '../style.css'
 
 const Users = () => {
     const [users, setUsers] = useState([]);
@@ -26,6 +27,13 @@ const Users = () => {
                     <li>address,suite: {userDetails.address.suite}</li>
                     <li>address,city: {userDetails.address.city}</li>
                     <li>address,zipcode: {userDetails.address.zipcode}</li>
+                    <li>address,zipcode,geo,lat: {userDetails.address.geo.lat}</li>
+                    <li>address,zipcode,geo,lng: {userDetails.address.geo.lng}</li>
+                    <li>phone: {userDetails.phone}</li>
+                    <li>website: {userDetails.website}</li>
+                    <li>company,name: {userDetails.company.name}</li>
+                    <li>company,catchPhrase: {userDetails.company.catchPhrase}</li>
+                    <li>company,bs: {userDetails.company.bs}</li>
                 </ul>
             </div>}
             <hr/>
