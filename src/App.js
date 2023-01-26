91fca42c339d23260cd14ff20132c66af3792f02
TesliukK <teslukkirill@gmail.com>
2023-01-26 13:06:25 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import {Route, Routes} from "react-router-dom";
+
+import {Header} from "./components";
+
+import HomePage from "./pages/HomePage/HomePage";
+import TodosPage from "./pages/TodosPage/TodosPage";
+import AlbumsPage from "./pages/AlbumsPage/AlbumsPage";
+import CommentsPage from "./pages/CommentsPage/CommentsPage";
+
 
 const App = () => {
-  return (
-    <div className="App">
+    return (
+        <div>
+            <Header/>
+            <Routes>
+                <Route path={'/'} element={<HomePage/>}/>
+                <Route path={'todos'} element={<TodosPage/>}/>
+                <Route path={'albums'} element={<AlbumsPage/>}/>
+                <Route path={'comments'} element={<CommentsPage/>}/>
+            </Routes>
+        </div>
 
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export * from './AlbumsPage/AlbumsPage';
+export * from './HomePage/HomePage'
+export * from './TodosPage/TodosPage';
+export * from './CommentsPage/CommentsPage';
