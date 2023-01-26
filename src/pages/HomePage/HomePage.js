75f327b485f054ce81753e5f22b54706a0310fb5
TesliukK <teslukkirill@gmail.com>
2023-01-26 16:32:19 +0200

    home.img

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+import css from './home.module.css'
 const HomePage = () => {
     return (
-        <div>
-            HomePage
+        <div className={css.home}>
+            <img src="https://t4.ftcdn.net/jpg/03/61/52/11/360_F_361521131_tvclR3GrsVQBFVsUe1EPNFgH2MWIN1w7.jpg" alt="welcome"/>
         </div>
     );
 };
