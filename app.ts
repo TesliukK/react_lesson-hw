c73f80f807c0bfb3bb840fe797263cddc4a503a2
TesliukK <teslukkirill@gmail.com>
2023-02-14 20:35:14 +0200

    done hw

--- app.ts
+++ app.ts
@@ -3,22 +3,22 @@ interface IUser {
     age: number,
     gender: string
 }
- const user:IUser = {name:'max',age:18,gender:'male'}
-console.log(user);
 
-
-const sum = (a: number, b: number) => {
-    return a + b
+const user:IUser = {
+    name:"Max",
+    age:18,
+    gender:'male'
 }
-console.log(sum(2, 5));
 
-const showSum = (a: number, b: number) => {
+function sum(a:number,b:number):number{
+    return a+b
+}
+function showSum(a:number,b:number):void{
     console.log(a + b);
 }
-showSum(2, 3)
 
-const incAge = (someUser:IUser, inc:number)=>{
+function incAge(someUser:IUser, inc:number):IUser{
     someUser.age+=inc
     return someUser
 }
-console.log(incAge(user, 2));
+
--- spaceX.ts
+++ spaceX.ts
@@ -1,3 +1,18 @@
+interface ICores {
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: string
+
+    }
+}
+
+interface IPayloads {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+
 interface ISpaceX {
     mission_name: string,
     launch_date_local: string,
@@ -11,38 +26,10 @@ interface ISpaceX {
     rocket: {
         rocket_name: string,
         first_stage: {
-            cores: [
-                {
-                    flight: number,
-                    core: {
-                        reuse_count: number,
-                        status: string
-                    }
-                }
-            ]
+            cores:ICores []
         },
         second_stage: {
-            payloads: [
-                {
-                    payload_type: string,
-                    payload_mass_kg: number,
-                    payload_mass_lbs: number
-                }
-            ]
+            payloads: IPayloads[]
         }
     }
 }
-
-const spaceX: ISpaceX = {
-    mission_name: "Starlink-15 (v1.0)",
-    launch_date_local: "2020-10-24T11:31:00-04:00",
-    launch_site: {site_name_long: "Cape Canaveral Air Force Station Space Launch Complex 40"},
-    links: {article_link: null, video_link: "https://youtu/J442-ti-Dhg"},
-    rocket: {
-        rocket_name:"Falcon 9",
-        first_stage:{cores:[{flight:7,core:{reuse_count:6, status:"unknown"}}]},
-        second_stage:{payloads:[{payload_type:"Satellite", payload_mass_kg: 15400, payload_mass_lbs:33951.2}]}
-    }
-}
-
-console.log(spaceX);
